2214fa666fc8c14b48db9224ed7796ae7636c93c
Brody14 <stefania.spinas@gmail.com>
2023-02-10 14:36:25 +0100

    EDIT with reset

--- js/script.js
+++ js/script.js
@@ -1,42 +1,55 @@
-console.log('Ioannis Papapetrou')
+console.log("Ioannis Papapetrou");
 
 //l’utente clicca su un pulsante PLAY  per generare la una griglia quadrata 10 * 10
-const btnPlayElement = document.querySelector('.btn-play');
-const gridElement = document.querySelector('.grid');
+const btnPlayElement = document.querySelector(".btn-play");
+const gridElement = document.querySelector(".grid");
 
-btnPlayElement.addEventListener('click', function() {
-    //console.log('click');
-    btnPlayElement.classList.add('display-none')
+btnPlayElement.addEventListener("click", startGame);
+//console.log('click');
 
-    let gridSide = 10;
-    let cellNumber = gridSide **2
-    //console.log(gridSide, cellNumber)
+//FUNZIONI
 
-    for(let i = 0; i < cellNumber; i++) {
-        let cellInner = i + 1;
-        //console.log(cellInner)
+function startGame() {
+	resetGame();
+	let gridSide = 10;
+	let cellNumber = gridSide ** 2;
+	//console.log(gridSide, cellNumber)
 
-        let widthCalc = `calc(100% / ${gridSide})`
+	for (let i = 0; i < cellNumber; i++) {
+		let cellInner = i + 1;
+		//console.log(cellInner)
 
-        //creo elemento html
-        const cellElement = document.createElement('div');
-        cellElement.classList.add('cell')
-        cellElement.style.width = widthCalc;
+		//creo elemento html
 
-        //ogni casella deve contenere il numero corrispondente a partire da 1 fino a 100
-        cellElement.innerHTML = cellInner
-        //console.log(cellElement)
-        
-        //lo "appendo" nel dom
-        gridElement.appendChild(cellElement).addEventListener('click', function() {
-            
-            //Ogni volta che l’utente clicca su una delle caselle della griglia stampiamo in console il numero della casella cliccata 
-            //(se ci riusciamo, in caso contrario stampiamo un messaggio generico)
-           
-            console.log(cellInner)
-            
+        let widthCalc = `calc(100% / ${gridSide})`;
+
+		//creo elemento html
+		const cellElement = document.createElement("div");
+		cellElement.classList.add("cell");
+		cellElement.style.width = widthCalc;
+		//const cellElement = `<div class="cell" style="width: calc(100% / ${gridSide});" >${cellInner}</div>`;
+
+		//lo "appendo" nel dom
+        gridElement.appendChild(cellElement)
+		//gridElement.innerHTML += cellElement;
+
+		//ogni casella deve contenere il numero corrispondente a partire da 1 fino a 100
+		cellElement.innerHTML = cellInner
+	}
+
+	const cellElements = document.querySelectorAll(".cell")
+    
+    for(let i = 0; i < cellElements.length; i++) {
+        const cell = cellElements[i]
+
+        cell.addEventListener('click', function () {
+            console.log(i + 1)
+            cell.classList.add('bg-violet')
         })
     }
+	//console.log(cellElements);
+}
 
-})
-
+function resetGame() {
+	gridElement.innerHTML = "";
+}
